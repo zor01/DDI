78011195b1983e9950fcc922471aac9050b8b027
zor01 <victor_mariamoliner@gmail.com>
2021-11-12 09:35:23 +0100

    Bloque2_Rubrica2

---
+++ JSPELExample/src/main/java/com/jcg/jsp/el/Address.java
@@ -0,0 +1,18 @@
+package com.jcg.jsp.el;
+
+public class Address {
+
+	private String address;
+
+	public Address() {	
+		
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
